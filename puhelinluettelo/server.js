095ad7bc428569d8a31e5dae86c8677680421024
Sami <savasp@utu.fi>
2019-07-12 23:35:52 +0300

    Part 3.4. done

--- puhelinluettelo/server.js
+++ puhelinluettelo/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const db = require('./db')
+const db = require('./db');
 
-const data = db;
-const time = Date().toString();
+let data = db.persons;
 
 
 app.get("/info", (req, res) => {
-    return res.send(`Phonebook has ${data.persons.length} people in it <br> <br> ${time}`);
+    return res.send(`Phonebook has ${data.length} people in it <br> <br> ${Date().toString()}`);
 })
 
 app.get("/api/persons", (req, res) => {
@@ -16,17 +15,31 @@ app.get("/api/persons", (req, res) => {
 })
 
 app.get("/api/persons/:id", (req, res) => {
-    const id = req.params.id
-    const people = data.persons
+    const id = req.params.id;
+    const people = data;
     const person = people.filter(person => person.id == id)
 
     if(!person.length){
-        return res.status(404).send("No such person found")
+        return res.status(404).send("No such person found");
     }else{
-        return res.status(200).send(person[0])
+        return res.status(200).send(person[0]);
     }
 })
 
+
+app.delete("/api/persons/:id", (req, res) =>{
+    const id = req.params.id;
+    const people = data;
+
+    const filteredPeople = people.filter(person => person.id != id);
+    console.log(filteredPeople)
+    data = filteredPeople;
+    if(filteredPeople.length === people.length){
+        return res.status(404).send("person not found")    
+    }else{
+    return res.status(200).send(data)
+    }
+})
 app.listen(3001, () => {
     console.log("Piippiip")
 })

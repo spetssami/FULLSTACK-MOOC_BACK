8fb2ccad4f73f89e0f2ea96c7b69c258d8b844ad
Sami <savasp@utu.fi>
2019-07-12 23:19:41 +0300

    Part 3.3. done

--- puhelinluettelo/server.js
+++ puhelinluettelo/server.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const app = express();
 
 const data = JSON.parse(fs.readFileSync('db.json'));
-console.log(data.persons.length)
-const time = Date().toString()
+const time = Date().toString();
 
 
 app.get("/info", (req, res) => {
@@ -13,6 +12,16 @@ app.get("/info", (req, res) => {
 
 app.get("/api/persons", (req, res) => {
     return res.send(data);
+})
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const people = data.persons
+    console.log(people)
+    const person = people.filter((person) => person.id == id)
+    console.log(person[0])
+    return res.send(person[0])
 })
 
 app.listen(3001, () => {

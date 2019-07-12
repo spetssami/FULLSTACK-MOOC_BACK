1104265f39861e56b37e189894c84ef03fc48ce8
Sami <savasp@utu.fi>
2019-07-12 22:40:58 +0300

    Part 3.1. done

--- puhelinluettelo/server.js
+++ puhelinluettelo/server.js
@@ -1,12 +1,16 @@
-import express from 'express';
+const express = require('express');
+const fs = require('fs');
 const app = express();
 
+const data = JSON.parse(fs.readFileSync('db.json'));
 
 app.get("/", (req, res) => {
-    res.send("Puhelinluettelon API");
+    return res.send("Puhelinluettelon API");
 })
 
-
+app.get("/api/persons", (req, res) => {
+    return res.send(data);
+})
 
 app.listen(3001, () => {
     console.log("Piippiip")

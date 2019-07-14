bbe8c149676580abea4f43127d59a69d75b75690
Sami <savasp@utu.fi>
2019-07-14 18:03:32 +0300

    part 3.9. 3.10. and 3.11 done

---
+++ build/precache-manifest.9e28e71b727e922345a6267931fe534a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e930957d1eaae97cd2ad992d49c5a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15b547590d6ba55362c9",
+    "url": "/static/css/main.318b99f3.chunk.css"
+  },
+  {
+    "revision": "c94dd88d161eb293c84a",
+    "url": "/static/js/2.d6c792d0.chunk.js"
+  },
+  {
+    "revision": "15b547590d6ba55362c9",
+    "url": "/static/js/main.95abc448.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/precache-manifest.9e9585e31ba8eed5b2c9532b72add353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9c9767dff63996d09698f0c28664fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed6670ba1385b9f328ff",
+    "url": "/static/css/main.318b99f3.chunk.css"
+  },
+  {
+    "revision": "c94dd88d161eb293c84a",
+    "url": "/static/js/2.d6c792d0.chunk.js"
+  },
+  {
+    "revision": "ed6670ba1385b9f328ff",
+    "url": "/static/js/main.e12d5e45.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

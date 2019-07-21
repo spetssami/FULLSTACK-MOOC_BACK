49e37d34aaba592ab952cdeb940a8e1fc53695cc
Sami <savasp@utu.fi>
2019-07-21 16:57:49 +0300

    Part 3.18. done

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const Person = require('./models/person');
 
 const port = process.env.PORT;
 
-app.use(bodyPareser.json());
+
 app.use(express.static('build'))
+app.use(bodyPareser.json());
 app.use(cors());
 morgan.token('post', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :response-time ms :post', {
@@ -18,10 +19,24 @@ app.use(morgan(':method :url :status :response-time ms :post', {
 }));
 
 
+// const errorHandler = (error, request, response, next) => {
+//     console.error(error.message)
+//     if (error.name === 'CastError' && error.kind == 'ObjectId') {
+//       return response.status(400).send({ error: 'malformatted id' })
+//     } 
+//     next(error)
+// }
+// app.use(errorHandler)
 
+// const unknownEndpoint = (req, res) => {
+//     res.status(404).send({ error: 'unknown endpoint' })
+// }
+// app.use(unknownEndpoint);
 
 app.get("/info", (req, res) => {
-    return res.send(`Phonebook has ${data.length} people in it <br> <br> ${Date().toString()}`);
+    Person.find({}).then(response => {
+        return res.send(`Phonebook has ${response.length} people in it <br> <br> ${Date().toString()}`);
+    })
 })
 
 app.get("/api/persons", (req, res) => {
@@ -30,10 +45,20 @@ app.get("/api/persons", (req, res) => {
     });  
 });
 
+
+
+
 app.get("/api/persons/:id", (req, res) => {
     Person.findById(req.params.id).then(person => {
-        res.json(person.toJSON());
-    });  
+        if(person) {
+            res.json(person.toJSON());
+        }else {
+            res.status(404).end()
+        }
+    }).catch(error => {
+        console.log(error)
+        res.status(400).send({ error: 'malformatted id' })
+      })
 })
 
 app.post("/api/persons", (req, res) => {
@@ -65,6 +90,10 @@ app.delete("/api/persons/:id", (req, res) =>{
     })
     .catch(error => res.status(502).send({err: error}));
 })
+
+
+
+
 app.listen(port, () => {
     console.log("Piippiip")
 })

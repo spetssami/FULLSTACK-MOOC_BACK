8956e5d4336187f45c4c3136c43003ef71aef63a
Sami <savasp@utu.fi>
2019-07-23 22:58:03 +0300

    Part 3.21 done

---
+++ build/precache-manifest.804818a2994c60abb06d06a30ec42fd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499a88028c4cd9f311ef17a8402ace9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6969cbbb99c8d20ff47",
+    "url": "/static/css/main.318b99f3.chunk.css"
+  },
+  {
+    "revision": "c94dd88d161eb293c84a",
+    "url": "/static/js/2.d6c792d0.chunk.js"
+  },
+  {
+    "revision": "a6969cbbb99c8d20ff47",
+    "url": "/static/js/main.85150da7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
